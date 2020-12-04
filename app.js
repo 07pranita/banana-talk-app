6fe4117fae4a2828a9e02131def5622cad7d405c
Pranita <pfulsundar8@gmail.com>
2020-12-05 00:35:50 +0530

    add JS code

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var btnTranslate = document.querySelector('.btn-translate');
+var inputText = document.querySelector('#txt-input');
+var outputText = document.querySelector('.output');
+
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text" + text;
+}
+
+function errorHandler(error){
+    console.log("Error occured " + error);
+    alert("Something wrong with server. Try again later.");
+}
+
+function clickHandler(){
+    var txtInput = inputText.value; //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(txtInput))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputText.innerText = translatedText;  //output
+    })
+    // outputText.innerText = "abs " + inputText.value;
+}
+
+btnTranslate.addEventListener("click", clickHandler);
