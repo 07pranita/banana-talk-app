19e2caecac5cd3eb6bbb4479f0c4213a2d6d8b81
Pranita <pfulsundar8@gmail.com>
2020-12-05 00:57:16 +0530

    update API url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var btnTranslate = document.querySelector('.btn-translate');
 var inputText = document.querySelector('#txt-input');
 var outputText = document.querySelector('.output');
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text){
     return serverURL + "?" + "text" + text;
